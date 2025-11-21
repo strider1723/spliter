86a19673058be3773636930f0c6acdf165ac5fa3
strider1723 <121473307+strider1723@users.noreply.github.com>
2025-11-21 21:34:54 +0530

    Update ExpenseService.java

--- src/main/java/com/yourname/billsplitter/demo/Services/ExpenseService.java
+++ src/main/java/com/yourname/billsplitter/demo/Services/ExpenseService.java
@@ -1,11 +1,21 @@
 package com.yourname.billsplitter.demo.Services;
 
-
 import com.yourname.billsplitter.demo.entity.Expense;
-
 import java.util.List;
 
 public interface ExpenseService {
+
     Expense addExpense(Expense expense);
+
     List<Expense> getAllExpenses();
+
+    List<Expense> getExpensesByCategory(String category);
+
+    List<Expense> getSortedExpenses(String sortBy);
+
+    Expense updateAmount(Long id, Double amount);
+
+    void softDeleteExpense(Long id);
+
+    Double getTotalSpentByPerson(Long personId);
 }
