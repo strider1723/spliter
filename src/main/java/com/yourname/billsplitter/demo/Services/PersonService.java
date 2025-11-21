f10701351a6838204bc44b583dab1cc7054344ed
strider1723 <121473307+strider1723@users.noreply.github.com>
2025-11-21 22:12:37 +0530

    Update PersonService.java

--- src/main/java/com/yourname/billsplitter/demo/Services/PersonService.java
+++ src/main/java/com/yourname/billsplitter/demo/Services/PersonService.java
@@ -1,11 +1,17 @@
 package com.yourname.billsplitter.demo.Services;
 
 import com.yourname.billsplitter.demo.entity.Person;
-
 import java.util.List;
 
 public interface PersonService {
+
     Person addPerson(Person person);
+
     List<Person> getAllPersons();
+
     Person getPersonById(Long id);
+
+    List<Person> searchByName(String name);
+
+    Person updatePersonName(Long id, String newName);
 }
