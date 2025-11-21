6bb2cf74703076292ebf5e3ff31e73adc7c90541
strider1723 <121473307+strider1723@users.noreply.github.com>
2025-11-21 22:11:45 +0530

    Update PersonRepository.java

--- src/main/java/com/yourname/billsplitter/demo/repository/PersonRepository.java
+++ src/main/java/com/yourname/billsplitter/demo/repository/PersonRepository.java
@@ -1,9 +1,10 @@
 package com.yourname.billsplitter.demo.repository;
 
-
-
-
 import com.yourname.billsplitter.demo.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {}
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByNameContainingIgnoreCase(String name);
+}
